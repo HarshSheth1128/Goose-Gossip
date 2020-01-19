fb8bc0c7a2e95b3a96525dc1c3abdee967d46000
Anjali Notaney <anjali@horizndev2s-MacBook-Pro.local>
2020-01-19 00:31:19 -0500

    created expanding search bar

--- src/common/SearchBar/index.jsx
+++ src/common/SearchBar/index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import './index.css';
+//import './index.css';
+import './SearchBar.css'
+import SearchIcon from '../../images/search.svg'
+//import { ReactComponent as SearchIcon } from '../../images/search.svg';
 
 const SearchBar = () => {
     return (
         <>
-            <div className="SearchBarRoot">
+        <div className="SearchBarRoot">
+            <div className="SearchBarWrapper">
+                <input className="SearchBarInput" type="text" placeholder="Type to search" />
+                <button className="SearchBarSubmit" >
+                    <img className="SearchIcon" src={SearchIcon} alt="Search"/>
+                </button>
             </div>
+        </div>
         </>
     );
 
